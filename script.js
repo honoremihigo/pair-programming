9f98fc56f6701a4ee70d4e6724101f2f1ffd4a65
Ben Toussaint <mbentoussaint@gmail.com>
2026-05-01 16:24:39 +0200

    I Started the whole Project. With the same logic

--- script.js
+++ script.js
@@ -1,38 +1,59 @@
-function formatDuration(seconds){
-    let second;
-    let minutes;
-    let hours;
-    let days;
-    let months;
-    if(typeof seconds !== "number" || seconds<0){
-        return "not a valid seconds";
-    }
+function formatDuration(secondsPassed){
 
-        if(seconds == 0){
-        return "now";
+    let seconds=0;
+    let minutes=0;
+    let hours=0;
+    let days=0;
+    let months=0
+    let years=0;
+    //Checking the eligibility of passed seconds inside the function
+    if(secondsPassed<0 || typeof (secondsPassed) !=="number" || secondsPassed.toString().split('').includes('.')){
+        return `"${secondsPassed}" is not valid seconds`;
     }
-    if(seconds<60){
-        return seconds>1? `${seconds} secs`: `${seconds} sec`;
-    }
-    if(seconds>=60 && seconds < 3600 ){
-        minutes = Math.floor(seconds/60)
-        second = seconds%60
-        return minutes > 1 ?`${minutes} minutes , ${second} secs`:`${minutes} minute , ${second} secs`;
-    }else if(seconds>=3600 && seconds <86400){
-        hours= Math.floor(seconds/3600)
-        minutes = Math.floor((seconds%3600) / 60)
-        second = (minutes%60)
-        return hours > 1 ?` ${hours} hours ,${minutes} minutes, ${second} secs`:`${hours} hour, ${minutes} minute(s), ${second} sec(s) `;
+    if(secondsPassed===0){
+        return `NOW`;
     }
 
 
+    //working for seconds production....
 
+    if(secondsPassed>0 && secondsPassed<60){
+        return secondsPassed==1? `${secondsPassed} second`:`${secondsPassed} seconds`;
+    }
 
+    //working for minutes production....
+    if(secondsPassed>=60 &&secondsPassed<3600){
+        minutes=Math.floor(secondsPassed/60);
+        seconds= Math.floor(secondsPassed%60);
+        return (minutes==1? `${minutes} minute`: `${minutes} minutes`) + (seconds==1? ` ${seconds} second`: ` ${seconds} seconds`);
+    }
+    //working for hours
+    if(secondsPassed>=3600 && secondsPassed<86400){
+        hours=Math.floor(secondsPassed/3600);
+        minutes=Math.floor((Math.floor(secondsPassed%3600))/60)
+        seconds= ((secondsPassed%3600)%60);
+        return (hours==1? `${hours} hour`: `${hours} hours`) + (minutes==1? ` ${minutes} minute`: ` ${minutes} minutes`) + (seconds==1? ` ${seconds} second`: ` ${seconds} seconds`);
+    }
+    //working for days...
+    if(secondsPassed>=86400 && secondsPassed<(86400*30)){
+        days=Math.floor(secondsPassed/86400);
+        hours= Math.floor((secondsPassed%86400)/3600);
+        minutes= Math.floor(((secondsPassed%86400)%3600)/60)
+        seconds= Math.floor(((secondsPassed%86400)%3600)%60)
+        return (days==1? `${days} day`: `${days} days`) + (hours==1? ` ${hours} hour`: ` ${hours} hours`) + (minutes==1? ` ${minutes} minute`: ` ${minutes} minutes`) + (seconds==1? ` ${seconds} second`: ` ${seconds} seconds`);
+    }
 
-    // if(seconds>=60 && seconds < 120){
-    //     const remainder = seconds - 60
-    //     return remainder > 0 ?`1 minutes and ${remainder} seconds`: `1 minute`
-    // }
+    //working for months...
+    if(secondsPassed>=(86400*30)){
+        months= Math.floor(secondsPassed/(86400*30));
+        days= Math.floor(secondsPassed%(86400*30)/86400);
+        hours=Math.floor(((secondsPassed%(86400*30))%86400)/3600);
+        minutes=Math.floor((((secondsPassed%(86400*30))%86400)%3600)/60);
+        seconds= (((secondsPassed%(86400*30))%86400)%3600)%60;
+        return (months==1?`${months} month`:`${months} months`) + (days==1? ` ${days} day`: ` ${days} days`) + (hours==1? ` ${hours} hour`: ` ${hours} hours`) + (minutes==1? ` ${minutes} minute`: ` ${minutes} minutes`) + (seconds==1? ` ${seconds} second`: ` ${seconds} seconds`);
+    }
 
 }
-console.log(formatDuration(86398));
+
+//calling and testing the function
+console.log(formatDuration( ));
